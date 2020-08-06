ee4af167d9d5215ea5959ae1d70295f5c2b3e1d1
zlgj <zlgj@zlgjdeMacBook-Air.local>
2020-08-06 10:23:56 +0800

    change

--- Example/LTScrollView/Lib/LTSimpleManager.swift
+++ Example/LTScrollView/Lib/LTSimpleManager.swift
@@ -53,6 +53,11 @@
         simpleRefreshTableViewHandle = handle
     }
     
+    /* add禁止左边table手势的index */
+    @objc public func fdoubleIndexArray(array:NSArray) {
+        self.tableView.doubleIndexArray = NSArray.init(array: array)
+    }
+    
     /* 代码设置滚动到第几个位置 */
     @objc public func scrollToIndex(index: Int)  {
         pageView.scrollToIndex(index: index)
--- Example/LTScrollView/Lib/LTTableView.swift
+++ Example/LTScrollView/Lib/LTTableView.swift
@@ -10,7 +10,33 @@ import UIKit
 
 
 class LTTableView: UITableView, UIGestureRecognizerDelegate {
+    
+    public var doubleIndexArray: NSArray = []
+    
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        if gestureRecognizer.isKind(of: UIPanGestureRecognizer.self) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.self) {
+            
+            let superTableP = gestureRecognizer.location(in: self)
+            if(superTableP.x <= 80) {
+                let cells = self.visibleCells
+                let content = cells.first?.contentView
+                let pageView = content?.subviews.first as? LTPageView
+                let scr = pageView?.subviews.first
+                if scr!.isKind(of: UIScrollView.self) {
+                    let scro = scr as! UIScrollView
+                    let index = Int(scro.contentOffset.x)/375 as Int
+                    if doubleIndexArray.contains(String(index)) {
+                        self.isScrollEnabled = false
+                        return false
+                    }
+                }
+                return true
+            }else{
+                return true
+            }
+        }
+        
         return gestureRecognizer.isKind(of: UIPanGestureRecognizer.self) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.self)
     }
 }
