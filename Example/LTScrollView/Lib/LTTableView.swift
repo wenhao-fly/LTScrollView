5c0260e6cf12f6daa870d018cfef52eacfe26a0a
zlgj <zlgj@zlgjdeMacBook-Air.local>
2020-08-07 10:44:08 +0800

    change3

--- Example/LTScrollView/Lib/LTTableView.swift
+++ Example/LTScrollView/Lib/LTTableView.swift
@@ -20,23 +20,29 @@ class LTTableView: UITableView, UIGestureRecognizerDelegate {
             let superTableP = gestureRecognizer.location(in: self)
             if(superTableP.x <= 80 && (superTableP.y >  44 + 49 + 185 * self.frame.size.width/375 - self.contentOffset.y)) {
                 let cells = self.visibleCells
-                let content = cells.first?.contentView
-                let pageView = content?.subviews.first as? LTPageView
-                let scr = pageView?.subviews.first
-                if scr!.isKind(of: UIScrollView.self) {
-                    let scro = scr as! UIScrollView
-                    let index = Int(scro.contentOffset.x)/375 as Int
-                    if doubleIndexArray.contains(String(index)) {
-                        self.isScrollEnabled = false
-                        return false
+                if cells.count == 1 {
+                    let content = cells.first?.contentView
+                    let pageView = content?.subviews.first as? LTPageView
+                    let scr = pageView?.subviews.first
+                    if scr!.isKind(of: UIScrollView.self) {
+                        let scro = scr as! UIScrollView
+                        let index = Int(scro.contentOffset.x)/Int(self.frame.size.width) as Int
+                        if doubleIndexArray.contains(String(index)) {
+                            self.isScrollEnabled = false
+                            return false
+                        }
                     }
                 }
-                return true
-            }else{
-                return true
             }
+            return true
         }
         
         return gestureRecognizer.isKind(of: UIPanGestureRecognizer.self) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.self)
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        self.isScrollEnabled = true
+        return hitView
     }
 }
